448e2d7350eff381d6fdac960246bc1727e404a0
Suraj <suraj.dadheech@costrategix.com>
2022-10-16 21:33:11 +0530

    log4j Commit

--- JavaProject/src/coreJavaCode/Main.java
+++ JavaProject/src/coreJavaCode/Main.java
@@ -1,21 +1,10 @@
 package coreJavaCode;
 
-import java.util.Set;
-import java.util.TreeSet;
-
 public class Main {
 
 	public static void main(String[] args) {
-		String newString="abcDeeeF12hhabcDeeeiiiijkl99z";
-//		System.out.println(str.replaceAll("^abcD+","YY"));	
-		
-		System.out.println(newString.replaceAll("e+i*j", "Y"));	
-		
-		
-		System.out.println("Abbc".matches("^[A-z]+"));
-		
-		
-		Set<Integer> l=new TreeSet<>();
+
+		System.out.println("Hello World");
 
 }
 }
